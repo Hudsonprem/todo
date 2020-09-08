9b7ca25d699b60a4c993fb305d85104633054a02
Hudson Daniel <hudsonprem97@gmail.com>
2020-09-08 16:38:25 +0530

    Updates

---
+++ precache-manifest.c6057f5ffbb797a0902d49cefd41236e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32828ac36ae3b5673d603b93ca1c86c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/todo/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "48b9e6f681ac66a30795",
+    "url": "/todo/static/js/main.41292a38.chunk.js"
+  },
+  {
+    "revision": "ca6384af61ea8d2512dc",
+    "url": "/todo/static/js/runtime-main.13151ce2.js"
+  }
+]);
